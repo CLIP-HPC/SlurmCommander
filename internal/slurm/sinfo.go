9a9f263023c12a800b83ff087f287917f55b06e3
Jager,Petar <petar.jager@imba.oeaw.ac.at>
2022-12-23 09:42:50 +0100

    remove leftover log.Printf

--- internal/slurm/sinfo.go
+++ internal/slurm/sinfo.go
@@ -1,7 +1,6 @@
 package slurm
 
 import (
-	"log"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,13 +12,11 @@ type SinfoJSON struct {
 	Nodes []openapi.V0039Node
 }
 
-//var gpuGresPattern = regexp.MustCompile(`^gpu\:([^\:]+)\:?(\d+)?`)
 var gpuGresPattern = regexp.MustCompile(`gpu:(.*:)?(\d+)\(.*`)
 
 func ParseGRES(line string) *int {
 	value := 0
 
-	log.Printf("GOT: %q\n", line)
 	gres := strings.Split(line, ",")
 	for _, g := range gres {
 		if !strings.HasPrefix(g, "gpu:") {
@@ -30,13 +27,6 @@ func ParseGRES(line string) *int {
 		if len(matches) == 3 {
 			value, _ = strconv.Atoi(matches[2])
 		}
-		//if len(matches) == 3 {
-		//	if matches[2] != "" {
-		//		value, _ = strconv.Atoi(matches[2])
-		//	} else {
-		//		value, _ = strconv.Atoi(matches[1])
-		//	}
-		//}
 	}
 
 	return &value
